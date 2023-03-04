f61e1f5c885fce128d36130375f547571a772651
Maryna Snitko <maryna_snitko@epam.com>
2023-03-05 00:58:45 +0400

    feat: populate tables with data
    product-service/src/functions/getProductsList/handler.ts

--- product-service/serverless.ts
+++ product-service/serverless.ts
@@ -2,17 +2,33 @@ import type { AWS } from '@serverless/typescript';
 import getProductsById from '@functions/getProductsById';
 import getProductsList from '@functions/getProductsList';
 
-
 const serverlessConfiguration: AWS = {
   service: 'product-service',
   frameworkVersion: '3',
-  plugins: ['serverless-esbuild'],
+  plugins: ['serverless-esbuild', 'serverless-offline'],
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
     stage: 'dev',
     region: 'eu-west-1',
-    
+
+    iamRoleStatements: [
+      {
+        Effect: 'Allow',
+        Action: [
+          'dynamodb:Query',
+          'dynamodb:Scan',
+          'dynamodb:GetItem',
+          'dynamodb:PutItem',
+          'dynamodb:UpdateItem',
+          'dynamodb:DeleteItem',
+        ],
+        Resource: [
+          `arn:aws:dynamodb:\${self:provider.region}:*:table/products`,
+          `arn:aws:dynamodb:\${self:provider.region}:*:table/stocks`
+        ]
+      },
+    ],
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
@@ -22,9 +38,53 @@ const serverlessConfiguration: AWS = {
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
     },
   },
-  
+  resources: {
+    Resources: {
+      ProductsTable: {
+        Type: 'AWS::DynamoDB::Table',
+        Properties: {
+          TableName: 'products',
+          AttributeDefinitions: [
+            { AttributeName: 'id', AttributeType: 'S' },
+            { AttributeName: 'title', AttributeType: 'S' },
+            { AttributeName: 'description', AttributeType: 'S' },
+            { AttributeName: 'imageUrl', AttributeType: 'S' },
+            { AttributeName: 'price', AttributeType: 'N' },
+          ],
+          KeySchema: [
+            { AttributeName: 'id', KeyType: 'HASH' },
+            { AttributeName: 'title', KeyType: 'RANGE' }
+          ],
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 1,
+            WriteCapacityUnits: 1
+          }
+        }
+      },
+      StocksTable: {
+        Type: 'AWS::DynamoDB::Table',
+        Properties: {
+          TableName: 'stocks',
+          AttributeDefinitions: [
+            { AttributeName: 'product_id', AttributeType: 'S' },
+            { AttributeName: 'score', AttributeType: 'N' }
+          ],
+          KeySchema: [
+            { AttributeName: 'product_id', KeyType: 'HASH' }],
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 1,
+            WriteCapacityUnits: 1
+          }
+        }
+      }
+    }
+  },
+
   // import the function via paths
-  functions: { getProductsList, getProductsById },
+  functions: {
+    getProductsList,
+    getProductsById,
+  },
   package: { individually: true },
   custom: {
     esbuild: {
